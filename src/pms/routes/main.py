78c5e4f14b2932bb80540c614bea149895c50e49
Emre Dinç <emredinc09@gmail.com>
2025-01-03 14:11:27 +0300

    solved problem of allowing to enroll to past courses

--- src/pms/routes/main.py
+++ src/pms/routes/main.py
@@ -30,75 +30,115 @@ def profile():
     return render_template("profile.html", user=user, benefits=benefits)
 
 
+
 @bp.route("/sessions")
 @login_required
 def sessions():
     user_email = current_user.id  # Dynamically get the logged-in user's email
-
     cur = get_cursor()
 
-    # Retrieve attended sessions with additional details
-    attended_query = """
-        SELECT ss.session_name, ss.session_date, ss.start_hour, ss.end_hour,
-               cs.class_level, CONCAT(cs.min_age, '-', cs.max_age) AS age_group,
-               pu.forename AS coach_forename, pu.surname AS coach_surname,
-               c.rating,
-               c.email AS coach_email
-        FROM swimming_session ss
-        JOIN swimmer_attend_session sas
-          ON ss.session_name = sas.session_name
-         AND ss.session_date = sas.session_date
-         AND ss.start_hour = sas.start_hour
-         AND ss.end_hour = sas.end_hour
-        LEFT JOIN class_session cs
-          ON ss.session_name = cs.session_name
-         AND ss.session_date = cs.session_date
-         AND ss.start_hour = cs.start_hour
-         AND ss.end_hour = cs.end_hour
-        LEFT JOIN coach c
-          ON ss.coach_email = c.email
-        LEFT JOIN pms_user pu
-          ON c.email = pu.email
-        WHERE sas.email = %s
-        ORDER BY ss.session_date DESC, ss.start_hour DESC;
-    """
-    cur.execute(attended_query, (user_email,))
-    attended_sessions = cur.fetchall()
+    try:
+        # **1. Past Sessions: Enrolled sessions that have already occurred**
+        past_sessions_query = """
+            SELECT ss.session_name, ss.session_date, ss.start_hour, ss.end_hour,
+                   cs.class_level, CONCAT(cs.min_age, '-', cs.max_age) AS age_group,
+                   pu.forename AS coach_forename, pu.surname AS coach_surname,
+                   c.rating,
+                   c.email AS coach_email
+            FROM swimming_session ss
+            JOIN swimmer_attend_session sas
+              ON ss.session_name = sas.session_name
+             AND ss.session_date = sas.session_date
+             AND ss.start_hour = sas.start_hour
+             AND ss.end_hour = sas.end_hour
+            LEFT JOIN class_session cs
+              ON ss.session_name = cs.session_name
+             AND ss.session_date = cs.session_date
+             AND ss.start_hour = cs.start_hour
+             AND ss.end_hour = cs.end_hour
+            LEFT JOIN coach c
+              ON ss.coach_email = c.email
+            LEFT JOIN pms_user pu
+              ON c.email = pu.email
+            WHERE sas.email = %s
+              AND (ss.session_date < CURRENT_DATE 
+                   OR (ss.session_date = CURRENT_DATE AND ss.end_hour < CURRENT_TIME))
+            ORDER BY ss.session_date DESC, ss.start_hour DESC;
+        """
+        cur.execute(past_sessions_query, (user_email,))
+        past_sessions = cur.fetchall()
 
-    # Retrieve available sessions with additional details
-    available_query = """
-        SELECT ss.session_name, ss.session_date, ss.start_hour, ss.end_hour,
-               cs.class_level, CONCAT(cs.min_age, ' - ', cs.max_age) AS age_group,
-               pu.forename AS coach_forename, pu.surname AS coach_surname,
-               c.rating,
-               c.email AS coach_email
-        FROM swimming_session ss
-        LEFT JOIN swimmer_attend_session sas
-          ON ss.session_name = sas.session_name
-         AND ss.session_date = sas.session_date
-         AND ss.start_hour = sas.start_hour
-         AND ss.end_hour = sas.end_hour
-         AND sas.email = %s
-        LEFT JOIN class_session cs
-          ON ss.session_name = cs.session_name
-         AND ss.session_date = cs.session_date
-         AND ss.start_hour = cs.start_hour
-         AND ss.end_hour = cs.end_hour
-        LEFT JOIN coach c
-          ON ss.coach_email = c.email
-        LEFT JOIN pms_user pu
-          ON c.email = pu.email
-        WHERE sas.email IS NULL
-        ORDER BY ss.session_date ASC, ss.start_hour ASC;
-    """
-    cur.execute(available_query, (user_email,))
-    available_sessions = cur.fetchall()
+        # **2. Upcoming Sessions: Enrolled sessions that are in the future**
+        upcoming_sessions_query = """
+            SELECT ss.session_name, ss.session_date, ss.start_hour, ss.end_hour,
+                   cs.class_level, CONCAT(cs.min_age, '-', cs.max_age) AS age_group,
+                   pu.forename AS coach_forename, pu.surname AS coach_surname,
+                   c.rating,
+                   c.email AS coach_email
+            FROM swimming_session ss
+            JOIN swimmer_attend_session sas
+              ON ss.session_name = sas.session_name
+             AND ss.session_date = sas.session_date
+             AND ss.start_hour = sas.start_hour
+             AND ss.end_hour = sas.end_hour
+            LEFT JOIN class_session cs
+              ON ss.session_name = cs.session_name
+             AND ss.session_date = cs.session_date
+             AND ss.start_hour = cs.start_hour
+             AND ss.end_hour = cs.end_hour
+            LEFT JOIN coach c
+              ON ss.coach_email = c.email
+            LEFT JOIN pms_user pu
+              ON c.email = pu.email
+            WHERE sas.email = %s
+              AND (ss.session_date > CURRENT_DATE 
+                   OR (ss.session_date = CURRENT_DATE AND ss.start_hour >= CURRENT_TIME))
+            ORDER BY ss.session_date ASC, ss.start_hour ASC;
+        """
+        cur.execute(upcoming_sessions_query, (user_email,))
+        upcoming_sessions = cur.fetchall()
+
+        # **3. Available Sessions: Future sessions not enrolled by the user**
+        available_sessions_query = """
+            SELECT ss.session_name, ss.session_date, ss.start_hour, ss.end_hour,
+                   cs.class_level, CONCAT(cs.min_age, '-', cs.max_age) AS age_group,
+                   pu.forename AS coach_forename, pu.surname AS coach_surname,
+                   c.rating,
+                   c.email AS coach_email
+            FROM swimming_session ss
+            LEFT JOIN swimmer_attend_session sas
+              ON ss.session_name = sas.session_name
+             AND ss.session_date = sas.session_date
+             AND ss.start_hour = sas.start_hour
+             AND ss.end_hour = sas.end_hour
+             AND sas.email = %s
+            LEFT JOIN class_session cs
+              ON ss.session_name = cs.session_name
+             AND ss.session_date = cs.session_date
+             AND ss.start_hour = cs.start_hour
+             AND ss.end_hour = cs.end_hour
+            LEFT JOIN coach c
+              ON ss.coach_email = c.email
+            LEFT JOIN pms_user pu
+              ON c.email = pu.email
+            WHERE sas.email IS NULL
+              AND (ss.session_date > CURRENT_DATE 
+                   OR (ss.session_date = CURRENT_DATE AND ss.start_hour >= CURRENT_TIME))
+            ORDER BY ss.session_date ASC, ss.start_hour ASC;
+        """
+        cur.execute(available_sessions_query, (user_email,))
+        available_sessions = cur.fetchall()
+
+    finally:
+        cur.close()
 
     return render_template(
         "sessions.html",
-        attended_sessions=attended_sessions,
+        past_sessions=past_sessions,
+        upcoming_sessions=upcoming_sessions,
         available_sessions=available_sessions,
     )
+
 
 
 def join_session_logic(session_name, session_date, start_hour, end_hour, user_email):
@@ -418,7 +458,6 @@ def get_rate_coach():
     start_hour = data.get("start_hour")
     end_hour = data.get("end_hour")
     user_email = current_user.id  # Using dynamic email
-    print("breakpoint")
     query = """
         SELECT rating, comment FROM coach_rating
         WHERE swimmer_email = %s
